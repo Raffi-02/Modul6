c5c94d4db3244ae9ead1f2d9df46b190e2b5e532
Raffi-002 <raffiihasann@gmail.com>
2025-03-29 20:37:33 +0700

    Challange

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <style>
+        body {
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Galeri Hewan</h1>
+    <table>
+        <tr>
+            <th>No.</th>
+            <th>Nama</th>
+            <th>Foto</th>
+            <th>Cari di Google</th>
+        </tr>
+        
+        // Lengkapi disini baris untuk masing masing hewan (Ayam, angsa, bebek, domba, kalkun, kambing, kelinci, kerbau, kuda, dan sapi )
+       
+    </table>
+</body>
+</html>
